388b8fda1d8a6f89cada5e8796e2664074495941
kaden1240 <pederka21@gmail.com>
2025-11-15 00:50:18 +0000

    Update streamlit_app.py with proper service account handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import requests
 from datetime import datetime, timedelta
 import pgeocode
 import json
-
+import os
 # -----------------------------
 # CONFIGURATION
 # -----------------------------
